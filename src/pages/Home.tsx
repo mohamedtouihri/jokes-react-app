38edf0526dccbf48d087caf8354cdfd91a8896c5
mohamedtouihri <twihrimelek9@gmail.com>
2025-09-20 23:45:56 +0100

    Finale Commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,9 @@
-import { LoginOutlined } from "@ant-design/icons";
-import { Button } from "antd";
-import { Link, Navigate } from "react-router-dom";
-import { LOGIN, REGISTER } from "../constants/routes/routes";
+
 
 export default function Home() {
   return (
-    <div
-      style={{
-        display: "flex",
-        gap: "10px",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "50px",
-      }}
-    >
-      <Button type="primary" icon={<LoginOutlined />} size="large">
-        <Navigate to={LOGIN} /> 
-        Login
-      </Button>
-      <Button type="primary" icon={<LoginOutlined />} size="large">
-        <Navigate to={REGISTER} />
-        Register
-      </Button>
+    <div> Home
+      
     </div>
   );
 }
