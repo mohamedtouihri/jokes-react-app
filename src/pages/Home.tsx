2c1c6f884c8da64ffe7118fcf2ab2ea82f8a402a
mohamedtouihri <twihrimelek9@gmail.com>
2025-09-20 23:19:49 +0100

    finale commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,18 @@
 import { LoginOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import { Link } from "react-router-dom";
+import { LOGIN, REGISTER } from "../constants/routes/routes";
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ display: "flex", gap: "10px", justifyContent: "center", marginTop: "20px" }}>
       <Button type="primary" icon={<LoginOutlined />}>
         Login
-        <Link to="/login" />
+        <Link to={LOGIN} />
       </Button>
       <Button type="primary" icon={<LoginOutlined />}>
         Register
-        <Link to="/register" />
+        <Link to={REGISTER} />
       </Button>
     </div>
   );
